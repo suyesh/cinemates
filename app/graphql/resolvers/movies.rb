edd45d414490828d66365908bb443a556047cb93
Suyesh Bhandari <suyeshb@gmail.com>
2018-09-09 00:21:32 -0400

    .

--- app/graphql/resolvers/movies.rb
+++ app/graphql/resolvers/movies.rb
@@ -7,6 +7,7 @@ module Resolvers
     argument :upcoming, types.Boolean, default_value: false
     argument :topRated, types.Boolean, default_value: false
     argument :lowRated, types.Boolean, default_value: false
+    argument :adult, types.Boolean, default_value: false
 
     type do
       name 'MoviesResults'
@@ -16,35 +17,49 @@ module Resolvers
       field :list, types[Types::MovieType]
     end
 
-    def get_movie_object(obj)
-      obj ? obj.movies : Movie
-    end
-
-    def get_total(obj)
-      obj ? obj.movies.count : Movie.count
-    end
-
-    def check_scopes(movies:, arguments:)
-      return movies.upcoming if arguments[:upcoming]
-      return movies.now_playing if arguments[:nowPlaying]
-      return movies.top_rated if arguments[:topRated]
-      return movies.low_rated if arguments[:lowRated]
-      movies
-    end
-
-    def build_movies(arguments:, object:)
-      movies = get_movie_object(object)
-      results = check_scopes(movies: movies, arguments: arguments)
-    end
-
-    def call(obj, args, _ctx)
-      movies = build_movies(arguments: args, object: obj).page(args[:page])
+    def call(obj, args, ctx)
+      set_instance_variables(obj, args, ctx)
+      movies = build_movies.page(args[:page])
       OpenStruct.new(
         pages: movies.total_pages,
         currentPage: args[:page],
-        total: get_total(obj),
+        total: get_total,
         list: movies
       )
     end
+
+    private
+
+    def set_instance_variables(obj, args, ctx)
+      @object = obj
+      @arguments = args
+      @context = ctx
+    end
+
+    def check_adults(object)
+      object.where(adult: @arguments[:adult])
+    end
+
+    def get_movie_object
+      @object ? check_adults(@object.movies) : check_adults(Movie)
+    end
+
+    def get_total
+      get_movie_object.count
+    end
+
+    def check_scopes
+      return @movies.upcoming if @arguments[:upcoming]
+      return @movies.now_playing if @arguments[:nowPlaying]
+      return @movies.top_rated if @arguments[:topRated]
+      return @movies.low_rated if @arguments[:lowRated]
+      @movies
+    end
+
+    def build_movies
+      @movies = get_movie_object
+      @movies = check_scopes
+    end
+
   end
 end
--- app/models/concerns/movie_concerns/scopes.rb
+++ app/models/concerns/movie_concerns/scopes.rb
@@ -3,10 +3,35 @@ module MovieConcerns
     extend ActiveSupport::Concern
 
     included do
-      scope :upcoming, -> { where('release_date BETWEEN ? AND ?', today_date, thirty_days_from_now).order('release_date DESC') }
-      scope :now_playing, -> { where('release_date BETWEEN ? AND ?', sixty_days_earlier, today_date).order('release_date DESC') }
-      scope :top_rated, -> { where('vote_average > ? AND vote_count > 1500', 6 ).order('vote_average DESC') }
-      scope :low_rated, -> { where('vote_average <= ? AND vote_count > 100', 5 ).order('vote_average ASC') }
+      scope :upcoming, -> {
+        where(
+          'release_date >= ? AND release_date <= ?',
+          today_date,
+          thirty_days_from_now
+        ).order('release_date DESC NULLS LAST')
+      }
+
+      scope :now_playing, -> {
+        where(
+          'release_date >= ? AND release_date <= ?',
+          sixty_days_earlier,
+          today_date
+        ).order('release_date DESC NULLS LAST')
+      }
+
+      scope :top_rated, -> {
+        where(
+          'vote_average > ? AND vote_count > 1500',
+          6
+        ).order('vote_average DESC NULLS LAST')
+      }
+
+      scope :low_rated, -> {
+        where(
+          'vote_average <= ? AND vote_count > 100',
+          5
+        ).order('vote_average ASC NULLS LAST')
+      }
     end
   end
 end
